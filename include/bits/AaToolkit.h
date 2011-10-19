d7f56c7bffdc145b50c188cc714713efbd0cf826
Aa! <aassif.benassarou@univ-reims.fr>
2011-10-19 16:36:14 +0200

    Win32 DLL import/export

---
+++ include/bits/AaToolkit.h
@@ -0,0 +1,18 @@
+#ifndef __AA_TOOLKIT__
+#define __AA_TOOLKIT__
+
+#ifdef _WIN32
+  #ifdef AaToolkit_EXPORTS
+    #define AA_TOOLKIT_API    __declspec(dllexport)
+    #define AA_TOOLKIT_EXTERN
+  #else
+    #define AA_TOOLKIT_API    __declspec(dllimport)
+    #define AA_TOOLKIT_EXTERN extern
+  #endif
+#else
+  #define AA_TOOLKIT_API
+  #define AA_TOOLKIT_EXTERN
+#endif
+
+#endif // __AA_TOOLKIT__
+
