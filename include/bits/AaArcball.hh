3ee2c4c3dd593298aa36a529e3d94c32e14c4cf9
Aa! <aassif.benassarou@univ-reims.fr>
2013-05-31 19:05:52 +0200

    Ajout de AaQuaternion et AaArcball.

---
+++ include/bits/AaArcball.hh
@@ -0,0 +1,57 @@
+#ifndef AA_ARCBALL__H
+#define AA_ARCBALL__H
+
+namespace Aa
+{
+
+////////////////////////////////////////////////////////////////////////////////
+// Aa::Arcball /////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+  class Arcball
+  {
+    private:
+      vec2  m_center;
+      float m_radius;
+      vec3  m_position;
+
+    private:
+      vec3 map (const vec2 & point) const
+      {
+        vec2 p = vec2 (+1.0f, -1.0f) * (point - m_center) / m_radius;
+        float d2 = p.length2 ();
+        return (d2 > 1.0f) ? vec3 (p / std::sqrt (d2), 0.0f) : vec3 (p, std::sqrt (1.0f - d2));
+      }
+
+    public:
+      Arcball () :
+        m_center (),
+        m_radius (0.0f),
+        m_position (Aa::vec<float> (0, 0, 1))
+      {
+      }
+
+      void resize (const uvec2 & d)
+      {
+        m_center   = 0.5f * d;
+        m_radius   = 0.5f * std::max (d[0], d[1]);
+        //m_position = vec<float> (0, 0, 1);
+      }
+
+      void init (const vec2 & p)
+      {
+        m_position = this->map (p);
+      }
+
+      quat operator() (const vec2 & p) const
+      {
+        vec3 p1 = m_position;
+        vec3 p2 = this->map (p);
+        return vec4 (CrossProd (p1, p2), 0.5f * DotProd (p1, p2));
+      }
+  };
+
+} // namespace Aa
+
+#endif // AA_ARCBALL__H
+
---
+++ include/bits/AaQuaternion.hh
@@ -0,0 +1,93 @@
+#ifndef AA_QUATERNION__H
+#define AA_QUATERNION__H
+
+namespace Aa
+{
+
+////////////////////////////////////////////////////////////////////////////////
+// Aa::Q<T> ////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+  template <typename T>
+  class Q : public V<T, 4>
+  {
+    public:
+      inline
+      Q (const M2<T, 3> & m = M2<T, 3> ()) :
+        V<T, 4> ()
+      {
+        (*this) = m;
+      }
+
+      inline
+      Q (const V<T, 4> & v) :
+        V<T, 4> (v.normalize ())
+      {
+      }
+
+      inline
+      Q & operator= (const M2<T, 3> & m)
+      {
+        T m00 = m (0, 0), m01 = m (0, 1), m02 = m (0, 2);
+        T m10 = m (1, 0), m11 = m (1, 1), m12 = m (1, 2);
+        T m20 = m (2, 0), m21 = m (2, 1), m22 = m (2, 2);
+
+        if (m00 + m11 + m22 > 0)
+        {
+          T t = 1 + m00 + m11 + m22;
+          (*this) = (0.5f * std::sqrt (t)) * vec<T> (m12 - m21, m20 - m02, m01 - m10, t);
+        }
+        else if (m00 > m11 && m00 > m22)
+        {
+          T t = 1 + m00 - m11 - m22;
+          (*this) = (0.5f * std::sqrt (t)) * vec<T> (t, m01 + m10, m20 + m02, m12 - m21);
+        }
+        else if (m11 > m22)
+        {
+          T t = 1 - m00 + m11 - m22;
+          (*this) = (0.5f * std::sqrt (t)) * vec<T> (m01 + m10, t, m12 + m21, m20 - m02);
+        }
+        else
+        {
+          T t = 1 - m00 - m11 + m22;
+          (*this) = (0.5f * std::sqrt (t)) * vec<T> (m20 + m02, m12 + m21, t, m01 - m10);
+        }
+
+        return (*this);
+      }
+
+      inline
+      M2<T, 3> matrix () const
+      {
+        //std::cout << "length: " << this->length () << std::endl;
+
+        T x = (*this) [0];
+        T y = (*this) [1];
+        T z = (*this) [2];
+        T w = (*this) [3];
+
+        T xw = 2 * x * w, x2 = 2 * x * x;
+        T yw = 2 * y * w, xy = 2 * x * y, y2 = 2 * y * y;
+        T zw = 2 * z * w, xz = 2 * x * z, yz = 2 * y * z, z2 = 2 * z * z;
+
+        return mat (vec (1 - y2 - z2,     xy - zw,     xz + yw),
+                    vec (    xy + zw, 1 - x2 - z2,     yz - xw),
+                    vec (    xz - yw,     yz + xw, 1 - x2 - y2));
+      }
+
+#if 0
+      inline
+      operator M2<T, 3> () const
+      {
+        return this->matrix ();
+      }
+#endif
+  };
+
+  typedef Q<float>   quat;
+  typedef Q<double> dquat;
+
+} // namespace Aa
+
+#endif // AA_QUATERNION__H
+
